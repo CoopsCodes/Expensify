4cf18f5250d1758b6b7bce84222aa56bead0cde0
Cooper Viktor <im.cooperviktor@gmail.com>
2020-09-16 13:52:37 +1000

    adding scss for styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
 	return (
 		<div className="App">
-			<h1>Hell Test</h1>
+			<h1>HelloWORLD</h1>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css/normalize.css";
-import "./index.scss";
+import "./App.scss";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
